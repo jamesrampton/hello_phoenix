b83da9a58f0e78ac52bd9fd2076e086bb5e7c38c
James Rampton <me@jamesrampton.com>
2022-06-04 18:52:18 +0100

    add some flash messages

--- lib/hello_phoenix_web/controllers/page_controller.ex
+++ lib/hello_phoenix_web/controllers/page_controller.ex
@@ -6,6 +6,8 @@ defmodule HelloPhoenixWeb.PageController do
   def index(conn, _params) do
     conn
     |> put_root_layout("admin.html")
+    |> put_flash(:info, "Welcome to Phoenix from flash info!")
+    |> put_flash(:error, "Let's pretend we have an error.")
     |> render("index.html")
   end
 end
