91c5e1ff4bcd24e57bbc89af2c0c7df7561c9017
James Rampton <me@jamesrampton.com>
2022-06-04 13:15:52 +0100

    use a plug inside a controller

--- lib/hello_phoenix_web/controllers/page_controller.ex
+++ lib/hello_phoenix_web/controllers/page_controller.ex
@@ -1,6 +1,8 @@
 defmodule HelloPhoenixWeb.PageController do
   use HelloPhoenixWeb, :controller
 
+  plug HelloPhoenixWeb.Plugs.Locale, "de"
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
