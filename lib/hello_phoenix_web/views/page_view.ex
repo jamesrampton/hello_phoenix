bed7515efdbf91134d872c56cb1824fd525312b5
James Rampton <me@jamesrampton.com>
2022-06-05 10:38:33 +0100

    define a local component

--- lib/hello_phoenix_web/views/page_view.ex
+++ lib/hello_phoenix_web/views/page_view.ex
@@ -1,3 +1,10 @@
 defmodule HelloPhoenixWeb.PageView do
   use HelloPhoenixWeb, :view
+  use Phoenix.Component
+
+  def country(assigns) do
+    ~H"""
+    <p>You live in <%= assigns.country %></p>
+    """
+  end
 end
